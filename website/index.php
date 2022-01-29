4a984584c18b4dda306ea1ad088c328746f9b353
dylou <dylan.innou@epita.fr>
2022-01-29 18:38:44 +0100

    [+] website

---
+++ website/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>epilearn</title>
+</head>
+
+<body>
+    <!--Header-->
+    <?php include 'header.php'; ?>
+
+    <!--Menu-->
+    <?php include 'menu.php'; ?>
+    
+    <!--Project description-->
+    
+    <!--Footer-->
+    <?php include 'footer.php'; ?>
+</body>
+</html>
---
+++ website/menu.php
@@ -0,0 +1,9 @@
+<body>
+    <nav>
+        <u1>
+            <li> Home </li>
+            <li> Progress </li>
+            <li> About </li>
+        <u1>
+    </nav>
+</body>
