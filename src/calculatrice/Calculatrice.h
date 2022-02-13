39fc454b9414a2f18ebc606cf7d0d60d5197c858
thomas.szabo <thomas.szabo@epita.fr>
2022-02-13 14:10:21 +0100

    [+] fix pour priorité des opérations

--- src/calculatrice/Calculatrice.h
+++ src/calculatrice/Calculatrice.h
@@ -8,6 +8,7 @@
 
 int findpivot(const char* chain,int size);
 int power(int a, int b);
+int sign(const char* chaine,int size);
 double calculR(const char* chain,int size,int x);
 double calcul(const char* chaine);
 void decimal(int d, double res);
