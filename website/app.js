4b281151197445547027a2dfff95eccb684ac1ec
dylou <dylan.innou@epita.fr>
2022-03-13 20:27:03 +0100

    [+] website

--- website/app.js
+++ website/app.js
@@ -1,3 +1,9 @@
+/*
+ * Author: Dylan INNOU
+ * Access token: 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
+ */
+
+
 /// Animated logo 
 
 const text = document.querySelector('.fancy');
@@ -35,3 +41,42 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+/// Github API
+
+const divResult = document.getElementById('scroll')
+
+async function pullRequest() {
+    const headers = {
+        'Authorization' : `Token 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`
+    }
+
+    const url = 'https://api.github.com/search/issues?q=+type:pr+org:Epilearns'
+
+    const response = await fetch(url, {
+        "method": "GET",
+        "headers": headers
+    })
+
+    const result = await response.json()
+
+    result.items.forEach(i => {
+        /*const pr = document.createElement('div')
+        //pr.textContent = i.title + '\n' + i.body
+
+        const title = document.createElement('p')
+        title.textContent = i.title
+
+        const content = document.createElement('p')
+        content.textContent = i.body
+
+        pr.appendChild(title)
+        pr.appendChild(content)*/
+        const pr = document.createElement("a")
+        pr.textContent = i.title
+        divResult.appendChild(pr)
+        divResult.appendChild(document.createElement('br'))
+    });
+}
+
+pullRequest()
