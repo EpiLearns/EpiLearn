ae3cbbb069c22c23ba2e373918d730d3752040e4
Alexandre <alexandre.bao@epita.fr>
2022-04-19 00:14:50 +0200

    pas de changement (j'ai mieux indenter le code)

--- src/calculator/node.c
+++ src/calculator/node.c
@@ -6,53 +6,63 @@
 #include <math.h>
 #include <string.h>
 
-
-
-node* calculator_createNode(void){
-    node* out = malloc(sizeof(node));
+node *calculator_createNode(void)
+{
+    node *out = malloc(sizeof(node));
     out->left = NULL;
     out->right = NULL;
-    out->operator = NULL;
+    out->operator= NULL;
     out->value = 0;
-    return out; 
+    return out;
 }
 
-double calculator_evaluate(node* head){
-    if(head == NULL) return 0;
-    
-    //calculator_evaluate operator first, value only if operator is null
-    if(head->operator == NULL){
+double calculator_evaluate(node *head)
+{
+    if (head == NULL)
+        return 0;
+
+    // calculator_evaluate operator first, value only if operator is null
+    if (head->operator== NULL)
+    {
         double value = head->value;
         free(head);
         return value;
     }
-    else{
-        double (*operator) (double, double) = head->operator;
-        node* left = head->left;
-        node* right = head->right;
+    else
+    {
+        double (*operator)(double, double) = head->operator;
+        node *left = head->left;
+        node *right = head->right;
         free(head);
         return operator(calculator_evaluate(left), calculator_evaluate(right));
     }
 }
 
-double calculator_add(double a, double b){
+double calculator_add(double a, double b)
+{
     return a + b;
 }
-double calculator_sub(double a, double b){
+double calculator_sub(double a, double b)
+{
     return a - b;
 }
-double calculator_mult(double a, double b){
-    return a*b;
+double calculator_mult(double a, double b)
+{
+    return a * b;
 }
-double calculator_divi(double a, double b){
-    if (b == 0){
+double calculator_divi(double a, double b)
+{
+    if (b == 0)
+    {
         printf("$: Warning: calculator_division by 0. This will return 0 as fallback.\n");
         return 0;
     }
-    float test = (float) a/b;
-    if (fabs(test - a/b) > 0.00001) printf("$: Warning: calculator_division will return rounded down integer value.\n");
-    return a/b;
+    float test = (float)a / b;
+    if (fabs(test - a / b) > 0.00001)
+        printf("$: Warning: calculator_division will return rounded down integer value.\n");
+    return a / b;
 }
-double calculator_power(double a, double b){
-    return pow(a,b);
+double calculator_power(double a, double b)
+{
+    return pow(a, b);
 }
--- src/calculator/parser.c
+++ src/calculator/parser.c
@@ -4,32 +4,35 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-char* calculator_replaceWord(char* s, char* oldW,char* newW)
+char *calculator_replaceWord(char *s, char *oldW, char *newW)
 {
-    char* result;
+    char *result;
     int i, cnt = 0;
     int newWlen = strlen(newW);
     int oldWlen = strlen(oldW);
-  
+
     // Counting the number of times old word
     // occur in the string
-    for (i = 0; s[i] != '\0'; i++) {
-        if (strstr(&s[i], oldW) == &s[i]) {
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        if (strstr(&s[i], oldW) == &s[i])
+        {
             cnt++;
-  
+
             // Jumping to index after the old word.
             i += oldWlen - 1;
         }
     }
-  
+
     // Making new string of enough length
-    result = (char*)malloc(i + cnt * (newWlen - oldWlen) + 1);
-  
+    result = (char *)malloc(i + cnt * (newWlen - oldWlen) + 1);
+
     i = 0;
-    while (*s) {
+    while (*s)
+    {
         // compare the substring with the result
-        if (strstr(s, oldW) == s) {
+        if (strstr(s, oldW) == s)
+        {
             strcpy(&result[i], newW);
             i += newWlen;
             s += oldWlen;
@@ -37,14 +40,15 @@ char* calculator_replaceWord(char* s, char* oldW,char* newW)
         else
             result[i++] = *s++;
     }
-  
+
     result[i] = '\0';
     return result;
 }
 
-int calculator_checkSyntax(char* string){
+int calculator_checkSyntax(char *string)
+{
 
-    //Check syntax, right now only checks if the characters are valid.
+    // Check syntax, right now only checks if the characters are valid.
     /*
     TODO:
         - Check parenthesis as valid
@@ -52,90 +56,104 @@ int calculator_checkSyntax(char* string){
     */
     char math[] = "x.+-*/^()1234567890";
     char alphabet[] = "abcdefghijklmnopqrstuvwyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char* checkA = strpbrk(string, alphabet);
-    char* checkB = strpbrk(string, math);
+    char *checkA = strpbrk(string, alphabet);
+    char *checkB = strpbrk(string, math);
 
-    if(checkA != NULL || checkB == NULL){
+    if (checkA != NULL || checkB == NULL)
+    {
         printf("$: INVALID COMMAND\n");
         printf("$: Only the math operation symbols:\n '+', '-', '*', '/', '^', '(', ')', numbers and commands are accepted as input.\n");
         return 0;
     }
-    else return 1;
+    else
+        return 1;
 }
 
-node* calculator_parseExpression(char *string){
-    node* current = calculator_createNode();
+node *calculator_parseExpression(char *string)
+{
+    node *current = calculator_createNode();
     int len = strlen(string);
     int parenthesisLevel = 0;
 
-    //First base case, check if the string contains only an integer number
-
+    // First base case, check if the string contains only an integer number
 
     char *check;
     double number = strtod(string, &check);
-    
 
-    if(check[0] == '\0'){
-        
+    if (check[0] == '\0')
+    {
+
         current->value = number;
-        
-        current->operator = NULL;
+
+        current->operator= NULL;
         return current;
     }
-    //Second "base" case, check if entire string is within parenthesis
-    //not really a base case if it calls itself again, but it behaves like one
+    // Second "base" case, check if entire string is within parenthesis
+    // not really a base case if it calls itself again, but it behaves like one
 
-    if(string[0] == '(' && string[len - 1] == ')' && calculator_findMatchingParenthesisIndexRight(string, 0) == len - 1){
+    if (string[0] == '(' && string[len - 1] == ')' && calculator_findMatchingParenthesisIndexRight(string, 0) == len - 1)
+    {
         string[len - 1] = '\0';
         free(current);
         current = calculator_parseExpression(&string[1]);
         return current;
     }
 
-    //Right to left, because tree is later evaluated left to right, so the tree constructor does it in reverse
-    //First pass is + and - signs, again because tree is evaluated in inverse order to its construction
+    // Right to left, because tree is later evaluated left to right, so the tree constructor does it in reverse
+    // First pass is + and - signs, again because tree is evaluated in inverse order to its construction
 
-    for(int i = len -1; i >= 0; i--){
+    for (int i = len - 1; i >= 0; i--)
+    {
         char c = string[i];
-        if (c == '(') parenthesisLevel--;
-        if (c == ')') parenthesisLevel++;
+        if (c == '(')
+            parenthesisLevel--;
+        if (c == ')')
+            parenthesisLevel++;
 
-        if (parenthesisLevel == 0 && (c == '+' || c == '-')){
+        if (parenthesisLevel == 0 && (c == '+' || c == '-'))
+        {
             string[i] = 0;
-            current->operator = (c == '+' ? calculator_add : calculator_sub);
+            current->operator=(c == '+' ? calculator_add : calculator_sub);
             current->left = calculator_parseExpression(string);
             current->right = calculator_parseExpression(&string[i + 1]);
             return current;
         }
     }
 
+    // Second pass for * and / signs
 
-    //Second pass for * and / signs
-
-    for(int i = len -1; i >= 0; i--){
+    for (int i = len - 1; i >= 0; i--)
+    {
         char c = string[i];
-        if (c == '(') parenthesisLevel--;
-        if (c == ')') parenthesisLevel++;
+        if (c == '(')
+            parenthesisLevel--;
+        if (c == ')')
+            parenthesisLevel++;
 
-        if (parenthesisLevel == 0 && (c == '*' || c == '/')){
+        if (parenthesisLevel == 0 && (c == '*' || c == '/'))
+        {
             string[i] = 0;
-            current->operator = (c == '*' ? calculator_mult : calculator_divi);
+            current->operator=(c == '*' ? calculator_mult : calculator_divi);
             current->left = calculator_parseExpression(string);
             current->right = calculator_parseExpression(&string[i + 1]);
             return current;
         }
     }
 
-    //Third pass for ^ operator
+    // Third pass for ^ operator
 
-    for(int i = len -1; i >= 0; i--){
+    for (int i = len - 1; i >= 0; i--)
+    {
         char c = string[i];
-        if (c == '(') parenthesisLevel--;
-        if (c == ')') parenthesisLevel++;
+        if (c == '(')
+            parenthesisLevel--;
+        if (c == ')')
+            parenthesisLevel++;
 
-        if (c == '^' && parenthesisLevel == 0){
+        if (c == '^' && parenthesisLevel == 0)
+        {
             string[i] = 0;
-            current->operator = calculator_power;
+            current->operator= calculator_power;
             current->left = calculator_parseExpression(string);
             current->right = calculator_parseExpression(&string[i + 1]);
             return current;
@@ -144,48 +162,61 @@ node* calculator_parseExpression(char *string){
     return NULL;
 }
 
-void calculator_removeSpaces(char* str){
-    
+void calculator_removeSpaces(char *str)
+{
 
     int oLen = strlen(str);
     int counter = 0;
-    for(int i = 0; i <= oLen; i++){
-        if(str[i] != ' ' && str[i] != '\n'){
+    for (int i = 0; i <= oLen; i++)
+    {
+        if (str[i] != ' ' && str[i] != '\n')
+        {
             str[counter] = str[i];
             counter++;
         }
-        else str[i] = '\0';
+        else
+            str[i] = '\0';
     }
     return;
 }
 
-int calculator_findMatchingParenthesisIndexRight(char* string, int index){
+int calculator_findMatchingParenthesisIndexRight(char *string, int index)
+{
     int len = strlen(string);
     int parentesisLevel = 1;
-    for (int i = index + 1; i < len; i++){
+    for (int i = index + 1; i < len; i++)
+    {
         char c = string[i];
-        if(c == '(') parentesisLevel++;
-        else if (c == ')'){
+        if (c == '(')
+            parentesisLevel++;
+        else if (c == ')')
+        {
             parentesisLevel--;
         }
-        if (parentesisLevel == 0){
+        if (parentesisLevel == 0)
+        {
             return i;
         }
     }
     return 0;
 }
 
-//Not used, but useful to have anyways
+// Not used, but useful to have anyways
 
-int calculator_findMatchingParenthesisIndexLeft(char* string, int index){
+int calculator_findMatchingParenthesisIndexLeft(char *string, int index)
+{
     int parentesisLevel = 1;
-    for (int i = index - 1; i >= 0; i--){
+    for (int i = index - 1; i >= 0; i--)
+    {
         char c = string[i];
-        if(c == '(') parentesisLevel--;
-        else if (c == ')'){
+        if (c == '(')
+            parentesisLevel--;
+        else if (c == ')')
+        {
             parentesisLevel++;
         }
-        if (parentesisLevel == 0){
+        if (parentesisLevel == 0)
+        {
             return i;
         }
     }
